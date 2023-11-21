4ea779b65621eaa460cdfecc4dbd09d0c9b7277e
Pranav Raju <pranavraju2020@gmail.com>
2023-11-21 19:34:20 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    todos.push(todo);
+    setTodo([...todo, todo]);
+    setTodo("");
+    console.log(todo);
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((todo, i) => i != index));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <form>
+        <input
+          type="text"
+          id="todo"
+          name="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <div className="todoItem">
+                <p>{todo}</p>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
